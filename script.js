845cf8f52b08d23668f4922b6d76c5e99eaced72
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-01-26 12:27:41 +1100

    implemented zoom feature using scroll wheel as well as added restrictions to where you can pan the map

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const map = document.querySelector('.world-map');
 const svg = document.querySelector('svg');
 
-// Global variables tracking how much the svg has been translated
-let offsetX = 0;
-let offsetY = 0;
+// Constants to track the max and min zoom factor and the zoom speed
+const MIN_ZOOM = 0.5;
+const MAX_ZOOM = 4;
+const ZOOM_SPEED = 10;
+
+// Global variables tracking how much the svg has been translated (panned)
+let panX = 0;
+let panY = 0;
+
+// Global variable tracking how much the map has been zoomed
+let scaleFactor = 1; 
 
 // Global boolean to track whether the map is being dragged
 let dragging = false;
 
+// Map click-and-drag behaviour
 map.addEventListener('mousedown', () => {
     dragging = true;
     map.classList.add('dragging');
@@ -20,13 +29,39 @@ map.addEventListener('mouseup', () => {
 
 map.addEventListener('mousemove', (event) => {
     if (dragging) {
-        panImage(event);
+        const svgCoords = svg.getBoundingClientRect();
+
+        // Set the pannable area for the map
+        const leftMax = -(svgCoords.width / 2);
+        const rightMax = svgCoords.width / 2; 
+        const topMax = -(svgCoords.height / 2); 
+        const bottomMax = svgCoords.height / 2;
+
+        panX += event.movementX;
+        panY += event.movementY;
+
+        // Restrict panning
+        panX = Math.min(Math.max(leftMax, panX), rightMax);
+        panY = Math.min(Math.max(topMax, panY), bottomMax);
+
+        updateSVGTransform();
     }
 });
 
-function panImage(event) {
-    offsetX += event.movementX;
-    offsetY += event.movementY;
+// Map zoom behaviour
+map.addEventListener('wheel', (event) => {
+    // Don't scroll the page
+    event.preventDefault();
 
-    svg.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+    scaleFactor += -ZOOM_SPEED / event.deltaY;
+
+    // Restrict zoom 
+    scaleFactor = Math.min(Math.max(MIN_ZOOM, scaleFactor), MAX_ZOOM);
+
+    updateSVGTransform();
+}); 
+
+function updateSVGTransform() {
+    svg.style.transform = `translate(${panX}px, ${panY}px) scale(${scaleFactor})`;
 }
+
